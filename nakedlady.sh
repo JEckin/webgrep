ec805a5284dea0099d67f6f016d830eb2a480f68
Justin Eckstädt <justin.eckstaedt@argus.info>
2020-09-07 13:30:35 +0200

    update

--- nakedlady.sh
+++ nakedlady.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-#https://nakedlady.ru
-
+#https://nakedlady.ru/
 t=0;
+mkdir -p nakedlady
+
 while [ 1 ]
 do
 	t=$(($t+1))
+	####
+	clear
+	echo "$t"
+	####
 	wget -qO- https://nakedlady.ru/index.php?p=$t > web.html
 	cat web.html | grep "jpg" | awk '{print $2'} | sed 's/href="//g' | sed 's/"//g'	> temp.txt
 	rm web.html
 	while read p; do
-		wget https://nakedlady.ru/$p
+		wget -q https://nakedlady.ru/$p && echo "Finished: "$p
 	done < temp.txt
+	mv *.jpg nakedlady
 	rm temp.txt
 done
